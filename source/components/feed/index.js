533c7fe943d5ad5798bdc20600e3020aecb8b7d0
Sergey Kurilchik <sergey.kurilchik@gmail.com>
2019-01-29 23:51:28 +0200

    Lesson 5

--- source/components/feed/index.js
+++ source/components/feed/index.js
@@ -3,16 +3,35 @@ import React, { Component } from 'react';
 import StatusBar from 'components/StatusBar';
 import Composer from 'components/Composer';
 import Post from 'components/Post';
+import Spinner from 'components/Spinner';
 
 import Styles from './styles.m.css';
 
 export default class Feed extends Component {
+    state = {
+        posts: [
+            {id: '123', comment: 'Hi there!', created: 1526825076849 },
+            {id: '234', comment: 'Hi, where are you?', created: 1526825076850 },
+        ],
+    }
+
     render() {
+        const { posts } = this.state;
+        const postsJSX = posts.map((post) => {
+            return (
+                <Post
+                    key = { post.id }
+                    { ...post }
+                />
+            );
+        });
+
         return (
             <section className = { Styles.feed }>
+                <Spinner isSpinning />
                 <StatusBar />
                 <Composer />
-                <Post />
+                { postsJSX }
             </section>
         );
     }
